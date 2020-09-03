df7e80c53fa3009ffcf9387d2ea0f67e270857f8
Florian GUILLOT <florian.guillot@leoo.fr>
2020-09-03 16:46:19 +0200

    add Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    listItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+} from "@material-ui/core"
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from "@material-ui/icons"
+
 
 const Navbar = () => {
     return (
-        <div>
+        <Box component="nav">
+            <AppBar>
+                <Toolbar>
+                    <ArrowBack>
 
-        </div>
+                    </ArrowBack>
+                </Toolbar>
+            </AppBar>
+        </Box >
     )
 }
 
