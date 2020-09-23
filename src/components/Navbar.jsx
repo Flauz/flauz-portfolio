2d7c173e5d4c254883cbf8e680d74072a5f93d6b
Florian GUILLOT <florian.guillot@leoo.fr>
2020-09-23 13:41:57 +0200

    add navbar and header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { makeStyles } from "@material-ui/core/styles"
+import MobilRightMenuSlider from "@material-ui/core/Drawer"
 import {
     AppBar,
     Toolbar,
     ListItem,
     IconButton,
-    listItemText,
     Avatar,
     Divider,
     List,
     Typography,
-    Box
+    Box,
+    ListItemIcon,
+    ListItemText
 } from "@material-ui/core"
 import {
     ArrowBack,
@@ -19,18 +22,101 @@ import {
     ContactMail
 } from "@material-ui/icons"
 
+// CSS STYLES
+const useStyles = makeStyles(theme => ({
+    menuSliderContainer: {
+        width: 250,
+        background: "#511",
+        height: "100%"
+    },
+    avatar: {
+        display: "block",
+        margin: "auto",
+        width: theme.spacing(13),
+        height: theme.spacing(13)
+    },
+    listItem: {
+        color: "tan"
+    }
+}))
+
+const menuItems = [
+    {
+        listIcon: <Home />,
+        listText: "Home"
+    },
+    {
+        listIcon: <AssignmentInd />,
+        listText: "Resume"
+    },
+    {
+        listIcon: <Apps />,
+        listText: "Portfolio"
+    },
+    {
+        listIcon: <ContactMail />,
+        listText: "Contacts"
+    }
+]
 
 const Navbar = () => {
-    return (
-        <Box component="nav">
-            <AppBar>
-                <Toolbar>
-                    <ArrowBack>
+    const [state, setState] = useState({
+        right: false
+    })
 
-                    </ArrowBack>
-                </Toolbar>
-            </AppBar>
-        </Box >
+    const toggleSlider = (slider, open) => () => {
+        setState({ ...state, [slider]: open })
+    }
+
+    const classes = useStyles()
+
+    const sideList = slider => (
+        <Box
+            className={classes.menuSliderContainer}
+            component="div"
+            onClick={toggleSlider(slider, false)}
+        >
+            <Avatar className={classes.avatar} src="" alt="" />
+            <Divider />
+            <List>
+                {menuItems.map((lsItem, key) => (
+                    <ListItem button key={key}>
+                        <ListItemIcon className={classes.listItem}>
+                            {lsItem.listIcon}
+                        </ListItemIcon>
+                        <ListItemText className={classes.listItem} primary={lsItem.listText} />
+
+                    </ListItem>
+                ))}
+
+            </List>
+        </Box>
+    )
+
+    return (
+        <>
+
+            <Box component="nav">
+                <AppBar position="static" style={{ background: "#222" }}>
+                    <Toolbar>
+
+                        <IconButton onClick={toggleSlider("right", true)}>
+                            <ArrowBack style={{ color: "tomato" }} />
+                        </IconButton>
+
+                        <Typography variant="h5" style={{ color: "tan" }}>
+                            Flauz's Portfolio
+                        </Typography>
+
+                        <MobilRightMenuSlider anchor="right" open={state.right} onClose={toggleSlider("right", false)}>
+                            {sideList("right")}
+                        </MobilRightMenuSlider>
+
+                    </Toolbar>
+                </AppBar>
+            </Box >
+
+        </>
     )
 }
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Header from './Header'
+import Particles from "react-particles-js"
 
 const Home = () => {
     return (
         <div>
+
             <Navbar />
+            <Particles
+                params={{
+                    particles: {
+                        number: {
+                            value: 30
+                        }
+                    }
+                }}
+                style={{ height: 100 }}
+            />
+            <Header />
         </div>
     )
 }
